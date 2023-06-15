85cbef49ccb86baaa35c71b37b36816d615a61e2
kiogeto <victoromondi7899@gmail.com>
2023-06-15 14:24:47 -0700

    hy

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * A C program that prins a line to the stardard error
+ *
+ * Return 1 (Success)
+ */
+{
+int main(void)
+	fput("and that piece of art is useful\" - Dora Korpar,
+2015-10-19\n", stdout);
+return (1);
+}
