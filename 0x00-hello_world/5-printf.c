47cc283bbd762665a94d5cdb4c47c7b3c2b97e0b
kiogeto <victoromondi7899@gmail.com>
2023-06-15 12:37:52 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+
+/**
+ * main - 5-printf.c
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{ 
+
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+
+}
