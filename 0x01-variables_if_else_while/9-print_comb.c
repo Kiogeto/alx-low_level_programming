f8dd37293f28d50c6efb18838d57bb1b679b7b3a
kiogeto <victoromondi7899@gmail.com>
2023-06-16 11:09:50 -0700

    fg

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - 9-print_comb.c
+ * Description - all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	putchar('0);
+
+	for (i = 1; i < 10; i++)
+	{
+	putchar(' ');
+	putchar(', ');
+	putchar('0' + i);
+
+	}
+	putchar('\n');
+
+	return (0)
+
+}
+
+
