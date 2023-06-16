480247da29e7d23a12db2f31919fb925364af1b6
kiogeto <victoromondi7899@gmail.com>
2023-06-16 10:00:46 -0700

    nmz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - 6-print_numberz.c
+ * Description -  single digit numbers of base 10 starting from 0
+ *
+ * Retutn: Always 0 (Success)
+ */
+
+int main(void)
+{
+	putchar('0');
+	putchar('1');
+	putchar('2');
+	putchar('3');
+	putchar('4');
+	putchar('5');
+	putchar('6');
+	putchar('7');
+	putchar('8');
+	putchar('9');
+	putchar('\n');
+
+	return (0);
+
+}
