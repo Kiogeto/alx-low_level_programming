de7efd699598e1483ca9ee3a8f4944f980299b31
kiogeto <victoromondi7899@gmail.com>
2023-06-20 04:33:51 -0700

    kl

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+/**
+ * main - 0-putchar.c
+ * Description - Write a program that prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c[] = "_putchar\n";
+
+	write(1, c, sizeof(0) - 1);
+
+	return (0);
+}
