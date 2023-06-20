48cb61680ed919b67529f8158ef2bf4ed5d36810
kiogeto <victoromondi7899@gmail.com>
2023-06-20 10:27:56 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char letter;
+
+	for (i = 0; i < 10; i++)
+	{
+
+	letter = 'a';
+
+	while (letter <= 'z')
+	{
+	_putchar(letter);
+
+	letter++;
+	}
+	}
+	_putchar('\n');
+}
