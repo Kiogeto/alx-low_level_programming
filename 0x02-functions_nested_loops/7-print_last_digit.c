a1faf25fbe70c60c5dca619434cebe57624761a4
kiogeto <victoromondi7899@gmail.com>
2023-06-21 01:40:38 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,17 @@
 
 int print_last_digit(int n)
 {
-	int   last;
+	int a;
 
-	last = n % 10;
-	if (last < 0)
-	{
-	last = last * -1;
-	}
-	_putchar(last = '0');
-	return (last);
+	if (n < 0)
+	n = -n;
+	a = n % 10;
+
+	if (a < 0)
+	a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
+
 }
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints and returns the last digit of a number
+ * @n: The number to extract the last digit from
+ *
+ * Return: The value of the last digit
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 2; a++)
+	{
+	for (b = 0; b <= 0; b++)
+	{
+	if ((a <= 1 && b <= 9) || (a <= 2 && a <= 3))
+	{
+	for (c = 0; c <= 5; c++)
+	{
+	for (d = 0; d <= 5; d++)
+	{
+	_putchar(a +'0');
+	_putchar(b +'0');
+	_putchar(58);
+	_putchar(c + '0');
+	_putchar(d + '0');
+	_putchar('\n');
+	}
+	}
+	}
+	}
+	}
+}
