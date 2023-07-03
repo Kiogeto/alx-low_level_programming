dfd6cec6fdcff61a886a982797375a2aebf1ad76
kiogeto <victoromondi7899@gmail.com>
2023-07-03 07:51:30 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
 char *_memset(char *s, char b, unsigned int n);
 
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -15,5 +19,7 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 
 void set_string(char **s, char *to);
+
+#endif /* MAIN_H */
 
 
